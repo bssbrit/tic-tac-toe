247ed275a67742f95dd6a2a31450068639183b92
Bruno <brunosabrito2@gmail.com>
2023-06-06 15:37:17 -0300

    made the main board function and organizade the object factorial for the players

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
-/*const gameboard = (() => {
-    const gameBoardarr = [
- “”, “” , “”, “” ,”” ,”” ,””, “”, “”
-]  
-   função addCheckSquare(square) {
-   consgameBoardarr[square]
-} 
-};
-})();*/
-
-const x = document.querySelector("h1");
-console.log(x);
-x.addEventListener("click", function () {
-  x.textContent = "hi";
-});
-
 const gameBoard = (() => {
   const gameBoardarr = ["", "", "", "", "", "", "", "", ""];
-  function addEachSquaretoDom() {
-    for (i = 0; i < gameBoardarr.length; i++) {
-      gameBoardarr[i] = document.getElementById(i);
-      console.log(gameBoardarr[i].textContent);
-      gameBoardarr[i].addEventListener("click", function () {
-        console.log("hi");
-      });
-    }
+
+  //this add each element of the array to the DOM and is responsive
+
+  for (i = 0; i < gameBoardarr.length; i++) {
+    gameBoardarr[i] = document.getElementById(i);
+    //console.log(gameBoardarr[i].textContent);
   }
-  addEachSquaretoDom();
+
+  //criar um for each para todos os elements adicionando o click evento de mudar o conteudo do texto do quadrado
+  gameBoardarr.forEach((elemento) => {
+    elemento.addEventListener("click", function () {
+      //aqui eu vou fazer ativar uma função "checkSquare" dai só vai ter ativar a função sempre que clicarmos
+
+      console.log("hi");
+    });
+  });
 
   return {
     gameBoardarr,
   };
 })();
-console.log(gameBoard.gameBoardarr[1]);
+
+const Player = (name, simbol) => {
+  const getName = () => name;
+
+  const getSimbol = () => simbol;
+
+  return { getName, getSimbol };
+};
+
+const Player1 = Player("Bruno", "x");
+const Player2 = Player("Chola", "o");
+
+/* 
+ To do: 
+  Lógica de getName e getSimbol
+  lógica para definir quando acabou uma rodada
+  lógica para definir quando acabou uma partida
+  botão de começar e reiniciar
+*/
