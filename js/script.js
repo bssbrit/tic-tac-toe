fa0abaa8eea883455315a6563559658e8b1e2365
Bruno <brunosabrito2@gmail.com>
2023-06-07 18:40:18 -0300

    made the simbol logic for the player, but i wasn't able to put it in a object factorial, which is kinda not the project objective

--- js/script.js
+++ js/script.js
@@ -12,10 +12,19 @@ const gameBoard = (() => {
   gameBoardarr.forEach((elemento) => {
     elemento.addEventListener("click", function () {
       //aqui eu vou fazer ativar uma função "checkSquare" dai só vai ter ativar a função sempre que clicarmos
-
+      checkSquare(simbolP1, simbolP2, elemento);
       console.log("hi");
     });
+
+    // função de checar se tem vencedor (podemos deixar ela dentro do click event)
   });
+
+  function checkSquare(simbolP1, simbolP2, elemento) {
+    if (elemento.textContent == "x" || elemento.textContent == "o")
+      alert("ERROR-ERROR-ERROR THIS SQUARE IS ALREADY FILLED");
+    else if (simbolP1 == "x") elemento.textContent = "x";
+    else if ((simbolP1 = "o")) elemento.textContent = "o";
+  }
 
   return {
     gameBoardarr,
@@ -30,13 +39,32 @@ const Player = (name, simbol) => {
   return { getName, getSimbol };
 };
 
-const Player1 = Player("Bruno", "x");
-const Player2 = Player("Chola", "o");
+//lógica para selecionar o simbolo do P1
+let simbolP1 = "";
+const botaoXP1 = document.getElementById("XplayerOne");
+const botaoOP1 = document.getElementById("OplayerOne");
+botaoXP1.addEventListener("click", function () {
+  simbolP1 = "x";
+  console.log(simbolP1);
+});
+botaoOP1.addEventListener("click", function () {
+  simbolP1 = "o";
+  console.log(simbolP1);
+});
 
-/* 
- To do: 
-  Lógica de getName e getSimbol
-  lógica para definir quando acabou uma rodada
-  lógica para definir quando acabou uma partida
-  botão de começar e reiniciar
-*/
+//Lógica para selecionar o simbolo do P2
+let simbolP2 = "";
+const botaoXP2 = document.getElementById("XplayerTwo");
+const botaoOP2 = document.getElementById("OplayerTwo");
+botaoXP2.addEventListener("click", function () {
+  simbolP2 = "x";
+  console.log(simbolP2);
+});
+botaoOP2.addEventListener("click", function () {
+  simbolP2 = "o";
+  console.log(simbolP2);
+});
+
+const Player1 = Player("Bruno", simbolP1);
+const Player2 = Player("Chola", simbolP2);
+console.log(Player2.getSimbol());
